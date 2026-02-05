9b94ddaa3fca5bfd426accca6cd3fe035af4fc38
Pedro Giamarco <pedrodaniel0020@gmail.com>
2026-02-05 01:59:37 -0300

    create backend server structure, add database postgres connection, create an environment with docker, hello world :)

---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import app from './config/api';
+import { prisma } from './config/db';
+import { env } from './config/env';
+
+prisma.$connect().then(() => {
+    console.log('Database connection established');
+}).catch((error) => {
+    console.error('Database connection error:', error);
+    // process.exit(1);
+});
+
+app.listen(env.PORT, () => {
+    console.log(`Server is running on port ${env.PORT}`);
+});
